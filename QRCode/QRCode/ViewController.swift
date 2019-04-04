c50eb530ac85f115b846f67eb7f01c36436d66a2
diep nguyen <>
2019-04-04 17:48:30 +0700

    init project

---
+++ QRCode/QRCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCode
+//
+//  Created by Diep Nguyen on 4/4/19.
+//  Copyright © 2019 Diep Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
